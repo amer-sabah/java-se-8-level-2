7b131f5d837cf53ba7add7ba2eea86bb9cdff785
Amer Sabah <amer.sabah1987@gmail.com>
2019-12-04 09:50:53 +0200

    filter and distinct

---
+++ src/jo/secondstep/javase8/level2/streamapi/ex1/Client10.java
@@ -0,0 +1,24 @@
+package jo.secondstep.javase8.level2.streamapi.ex1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Client10 {
+
+	public static void main(String[] args) {
+
+		// >>>>>>>>>>>>>>>>>>>>>>>> Items >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+		// Using Common Intermediate Operations
+		// distinct()
+
+		List<String> colors = Arrays.asList("Red","Black","Blue","Green", "Orange", "Yellow", "White", "Black","Blue");
+		
+		colors.stream().forEach(System.out::println);
+		
+		System.out.println("======================================");
+		
+		List<String> colors2 = Arrays.asList("Red","Black","Blue","Green", "Orange", "Yellow", "White", "Black","Blue");
+		
+		colors2.stream().distinct().forEach(System.out::println);
+	}
+}
---
+++ src/jo/secondstep/javase8/level2/streamapi/ex1/Client9.java
@@ -0,0 +1,28 @@
+package jo.secondstep.javase8.level2.streamapi.ex1;
+
+import java.util.List;
+import java.util.Map;
+
+import jo.secondstep.javase8.level2.Employee;
+import jo.secondstep.javase8.level2.EmployeeService;
+
+public class Client9 {
+
+	public static void main(String[] args) {
+
+		// >>>>>>>>>>>>>>>>>>>>>>>> Items >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+		// Using Common Intermediate Operations
+		// filter()
+
+		EmployeeService employeeService = new EmployeeService();
+		
+		List<Employee> employees = employeeService.getEmployees();
+		
+		Map<Employee, String> departments = employeeService.getDepartments();
+		
+		long count = employees.stream().filter(e -> departments.get(e).equals("Human Resources")).count();
+		
+		System.out.printf("%d employees in 'Human Resources' department\n", count);
+		employees.stream().filter(e -> departments.get(e).equals("Human Resources")).forEach(e-> System.out.println(e.getName()));
+	}
+}
